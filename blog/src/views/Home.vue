267f58a41f96d35a91956a3d93451e5920c32cc2
赵时杰 <zhaoshijie@dataocean.com.cn>
2021-02-25 18:43:58 +0800

    项目初始化

---
+++ blog/src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <img
+      alt="Vue logo"
+      src="../assets/logo.png"
+    >
+    <Header msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Header from '@/components/Header.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Header
+  }
+}
+</script>
